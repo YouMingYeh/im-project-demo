aad23c63dd36ee0740b8499edb93046514d5c757
YouMingYeh <b10705052@ntu.edu.tw>
2024-12-03 18:58:59 +0800

    refactor: comment out unused components in solution page

--- src/app/dashboard/data/solution/page.tsx
+++ src/app/dashboard/data/solution/page.tsx
@@ -1,9 +1,13 @@
 import Solution from "./solution";
+// import WorksheetPct from "./worksheet_pct";
+// import WorksheetTime from "./worksheet_time";
 
 export default function Page() {
   return (
     <div className="container py-10">
       <h1 className="mb-6 text-2xl font-bold">結果</h1>
+      {/* <WorksheetPct /> */}
+      {/* <WorksheetTime /> */}
       <Solution />
     </div>
   );
